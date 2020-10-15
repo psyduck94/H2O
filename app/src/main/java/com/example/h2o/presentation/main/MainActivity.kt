b43075c38698c360a08a7502c736e776d13137a2
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2020-10-15 19:27:48 -0300

    implementing progressInMl toolbar title

--- app/src/main/java/com/example/h2o/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/h2o/presentation/main/MainActivity.kt
@@ -35,7 +35,6 @@ class MainActivity : AppCompatActivity() {
 
         viewModel.fetchWaterLogFromLocalDb()
 
-        val waveLoadingView: WaveLoadingView
     }
 
     private fun initBindingConfig() {

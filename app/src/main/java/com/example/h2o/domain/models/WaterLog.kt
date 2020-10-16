27e59e86a7ee2e823870e30da71e109f27b8fc96
Luis Felipe Fagundes <luisfelipecf94@gmail.com>
2020-10-15 22:02:59 -0300

    fixing water progress in percentage bug

--- app/src/main/java/com/example/h2o/domain/models/WaterLog.kt
+++ app/src/main/java/com/example/h2o/domain/models/WaterLog.kt
@@ -1,17 +1,21 @@
 package com.example.h2o.domain.models
 
 import com.example.h2o.utils.GlobalConstants.Companion.DEFAULT_GOAL_OF_THE_DAY_VALUE
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class WaterLog(
     val goalOfTheDay: Int = DEFAULT_GOAL_OF_THE_DAY_VALUE,
     var progress: Int = 0,
     var progressInMl: String = "0/${goalOfTheDay*100}",
     var progressInPercentage: Int = 0,
-    val date: String
+    val date: String,
+    var dayOfTheWeek: String = ""
 ) {
     init {
         formatProgressInMl()
         formatProgressInPercentage()
+        formatDayOfTheWeek()
     }
 
     private fun formatProgressInMl() {
@@ -21,6 +25,10 @@ data class WaterLog(
     private fun formatProgressInPercentage() {
         val result = (progress.toFloat()/goalOfTheDay.toFloat())*100
         progressInPercentage = result.toInt()
+    }
+
+    private fun formatDayOfTheWeek() {
+        dayOfTheWeek = SimpleDateFormat("EEEE", Locale.getDefault()).format(Date())
     }
 }
 
